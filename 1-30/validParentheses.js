1652e11bde90424bf0843cd1a463dad326bf7d28
xieshuhong <364118360@qq.com>
2024-10-10 13:38:13 -0600

    reconstruct the file and added longestCommonPrefix in JavaScript

--- 1-30/validParentheses.js
+++ 1-30/validParentheses.js
@@ -5,13 +5,14 @@ function isBalanced(s) {
     const mapping = {')': '(', '}':'{', ']': '['};
 
     for (let letter of s) {
+        console.log('letter', letter, letter in mapping);
         if(letter in mapping) {
             let top_elements = stack.length > 0 ? stack.pop() : '#';
             if(mapping[letter] != top_elements) return false;
         } else {
             stack.push(letter);
         }
-
+        console.log('stack', stack);
     }
     return stack.length == 0;
 }
