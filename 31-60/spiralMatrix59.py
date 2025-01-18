caf2596e47a1ab4100f302de289f911eca12102a
xieshuhong <364118360@qq.com>
2025-01-17 22:14:56 -0700

    spiralMatrix59 in python

---
+++ 31-60/spiralMatrix59.py
@@ -0,0 +1,30 @@
+def spiralMatrix(n) -> list:
+    matrix = [[0] * n for _ in range(n)]
+    top, bottom, left, right, num = 0, n-1, 0, n-1, 1
+    
+    while left <= right:
+        for i in range(left, right + 1):
+            matrix[top][i] = num
+            num += 1
+        top += 1
+        
+        for i in range(top, bottom + 1):
+            matrix[i][right] = num
+            num += 1
+        right -= 1
+        
+        for i in range(right, left - 1, -1):
+            matrix[bottom][i] = num
+            num += 1
+        bottom -= 1
+        
+        for i in range(bottom, top - 1, -1):
+            matrix[i][left] = num
+            num += 1
+        left +=1
+    return matrix
+        
+n = 3
+print(spiralMatrix(n))
+
+
